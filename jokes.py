fba509b96ab0c4ead44412b8ee244994f9258e32
juicyfruit2 <134799673+juicyfruit2@users.noreply.github.com>
2023-07-06 15:38:36 +0200

    Add files via upload

---
+++ jokes.py
@@ -0,0 +1,12 @@
+# created a random joke generator using Python’s random module
+
+# imported the random function to help print out random jokes 
+import random
+
+# created a list to store my jokes 
+jokes = ["why was the broom late school: he over swept",
+"What did the one ocean say to the other ocean: sea you later",
+"what is the one tree you can hold in your hand : a palm tree "]
+
+# jokes will be printed out randomly 
+print(random.choice(jokes))
